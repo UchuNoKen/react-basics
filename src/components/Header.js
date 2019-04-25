9114e8caaf7df8308406412a1a73d227f3e18d00
UchuNoKen <xen.o.fite@gmail.com>
2019-04-25 13:22:47 -0400

    convert Header to class-based component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Header = () => {
-  return <div>Header</div>;
-};
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <div>Logo</div>
+        <input type="text" />
+      </header>
+    );
+  }
+}
 
 export default Header;
