87d68d36da2d0f2fbe4df6dfb1a340a3f8408c08
UchuNoKen <xen.o.fite@gmail.com>
2019-04-25 13:07:20 -0400

    rename index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// const App = () => {
-//   return <h1>Hello there</h1>;
-// };
-
-// ReactDOM.render(<App />, document.getElementById("root"));
-
-// Same as:
-// return React.createElement('h1', null, 'Hello there')
-
 const App = () => {
   return (
     <div>
@@ -20,9 +11,3 @@ const App = () => {
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
-// Same as:
-// return React.createElement('div', null,
-//           React.createElment('h1', {className: 'title'}, 'Hello there'),
-//             React.createElement('div', null, 'Gooday')
-//     );
