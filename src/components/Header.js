f8c0fd1193116b79f8cf09c256492f8c2ed1eac2
UchuNoKen <xen.o.fite@gmail.com>
2019-04-26 18:42:43 -0400

    bind this example

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,24 @@
 import React, { Component } from "react";
 
 class Header extends Component {
+  state = {
+    keywords: "Hello"
+  };
+
   inputChangeHandler(event) {
     // console.log("input change registered");
-    console.log(event.target.value);
+    // console.log(event.target.value);
+    this.setState({
+      keywords: event.target.value
+    });
   }
 
   render() {
+    console.log(this.state.keywords);
     return (
       <header>
         <div className="logo">Logo</div>
-        <input type="text" onChange={this.inputChangeHandler} />
+        <input type="text" onChange={this.inputChangeHandler.bind(this)} />
       </header>
     );
   }
