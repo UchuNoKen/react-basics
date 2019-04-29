f914a4433132540c737e485d3a36569415047bb0
UchuNoKen <xen.o.fite@gmail.com>
2019-04-29 17:45:29 -0400

    refactor to use css modules

--- src/components/NewsListItem.js
+++ src/components/NewsListItem.js
@@ -3,27 +3,8 @@ import { css } from "glamor";
 
 const NewsItem = ({ item }) => {
   // console.log(props);
-
-  let news_item = css({
-    color: "white",
-    fontFamily: "Helvetica",
-    padding: "20px",
-    boxSizing: "border-box",
-    borderBottom: "1px solid aliceBlue",
-    ":hover": {
-      color: "aliceBlue"
-    },
-    "@media(max-width: 500px)": {
-      color: "cadetBlue"
-    }
-  });
-
-  let item_grey = css({
-    background: "slateGrey"
-  });
-
   return (
-    <div className={`${news_item} ${item_grey}`}>
+    <div>
       <h3>{item.title}</h3>
       <div>{item.feed}</div>
     </div>
