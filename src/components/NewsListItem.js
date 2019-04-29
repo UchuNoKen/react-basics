9ea9f5c69d5aff76709ecfff89f4d17006f72054
UchuNoKen <xen.o.fite@gmail.com>
2019-04-29 17:17:28 -0400

    style with glamor css method

--- src/components/NewsListItem.js
+++ src/components/NewsListItem.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { css } from "glamor";
 
 const NewsItem = ({ item }) => {
   // console.log(props);
+
+  let news_item = css({
+    padding: "20px",
+    boxSizing: "border-box",
+    borderBottom: "1px solid grey",
+    ":hover": {
+      color: "darkSlateGrey"
+    }
+  });
+
+  let item_grey = css({
+    background: "aliceBlue"
+  });
+
   return (
-    <div>
+    <div {...news_item} {...item_grey}>
       <h3>{item.title}</h3>
       <div>{item.feed}</div>
     </div>
